fae4449f3501b51da9415f0f64011f13a532da33
zun <k5orkrj@gmail.com>
2025-07-10 14:25:52 +0900

    feat: add status field to distinguish draft and published posts

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -7,9 +7,18 @@ from django.db import models
 
 class Post(models.Model):
 
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     content = models.TextField()
+    statue = models.CharField(
+        max_length=2, # DF or PB
+        choices=Status.choices,
+        default=Status.DRAFT,
+    )
 
     published = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
