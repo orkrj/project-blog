6592b84e3ad53540f0f12f59321a35977bca950a
zun <k5orkrj@gmail.com>
2025-07-11 11:41:24 +0900

    feat: add author field (User module) to Blog model and create migration

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,6 @@
 from dataclasses import field
 
+from django.contrib.auth.models import User
 from django.utils import timezone
 
 from django.db import models
@@ -14,6 +15,11 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     content = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='blog_posts',
+    )
     statue = models.CharField(
         max_length=2, # DF or PB
         choices=Status.choices,
